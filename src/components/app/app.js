0f5acd242543425f713f93f09565df67e6d08741
ILYAS NASIROV <ilyasss1981@gmail.com>
2021-01-17 02:55:46 +0300

    step-1 lesson_110

---
+++ src/components/app/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {MainPage, CartPage} from '../pages';
+import AppHeader from '../app-header';
+import WithRestoService from '../hoc'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import Background from './food-bg.jpg';
+
+const App = ({RestoService}) => {
+    console.log(RestoService.getMenuItems())
+    return (
+        <Router>
+            <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+                <AppHeader total={50}/>
+                <Switch>
+                    <Route path='/' exact component={MainPage}/>
+                    <Route path='/cart' component={CartPage}/>
+                    <Route exact component={MainPage}/>
+                </Switch>
+                
+            </div>
+        </Router>
+    )
+}
+
+export default WithRestoService()(App);
